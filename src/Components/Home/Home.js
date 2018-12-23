0bd9220f952fec2a4a084a2a285cf35499346b66
Blake Rindlisbacher <blakebacher@gmail.com>
2018-12-22 21:49:03 -0700

    changed pictures and added scroll function

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,19 +5,33 @@ import Nav from './../Nav/Nav'
 import Footer from './../Footer/Footer'
 import './Home.css'
 
+import downarrow_blue from './../Images/downarrow_blue.png'
+
 
 export default class Home  extends Component {
+
+    handlescroll(){
+        window.scrollTo({
+            top: 620,
+            behavior: 'smooth'
+          });
+    }
+
     render() {
         return (
             <div>
                 <Nav/>
                 <body>
-                    <div id='home_maintitle'>
-                        <h1>Excellence in every complex.</h1>
+                    <h1 id='maintitle'>Immerse yourself in <span className='highlight'>paradise</span></h1>
+                    <h6 id='subtitle'>Located in West Maui, just minutes from dining and shopping in historic Lahaina town and Ka'anapali, you’ll find a very special Maui real estate community named Wailele Ridge.</h6>
+                    <div id='arrow_box' onClick={() => {this.handlescroll()}}>
+                        <img alt='' src={downarrow_blue} className='downarrow passing'/>
                     </div> 
                     <div id='background1'></div> 
-                    <div style={{height:'60vh'}}></div>
-                    <div id='background2'></div>  
+                    <div style={{height:'80vh'}}></div>
+                    <div id='background2'></div>
+                    <div style={{height:'80vh'}}></div>
+                    <div id='background3'></div>
                 </body>
                 <Footer/>
             </div>
