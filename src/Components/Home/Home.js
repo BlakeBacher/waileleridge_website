8e967e518e1b0886df93c002e708546fdda52370
Blake Rindlisbacher <blakebacher@gmail.com>
2019-01-26 09:40:36 -0700

    finished typewriter and started info boxes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Typing from 'react-typing-animation';
+
 
 
 import Nav from './../Nav/Nav'
@@ -33,25 +35,32 @@ export default class Home  extends Component {
     changePlace(e){
         this.setState({places: e.target.value})
     }
-
+    
     render() {
-        const content =()=>{
-            const message =["WELCOME TO MY WORLD","THIS IS MY WEBSITE","I AM AT YOUR SERVICE"];
-            let i =0 ;
-            let jsxArray = [];
-            for(i=0; i<message.length;i++){
-              jsxArray.push(
-                <div className='background'>
-                  <h4 className='typewriter'>
-                      {message[i]}    
-                  </h4>
-                </div>
-               );
-            }
-            return jsxArray;
-          }
-        return (
-            <div>
+        const AnimatedTypingComponent = () => (
+            <Typing loop cursorClassName='cursor'>
+              <span className='highlightblue'>places</span>
+              <Typing.Delay ms={2000} />
+              <Typing.Backspace count={15} />
+              <span className='highlightblue'>beaches</span>
+              <Typing.Delay ms={2000} />
+              <Typing.Backspace count={15} />
+              <span className='highlightblue'>parks</span>
+              <Typing.Delay ms={2000} />
+              <Typing.Backspace count={15} />
+              <span className='highlightblue'>shopping</span>
+              <Typing.Delay ms={2000} />
+              <Typing.Backspace count={15} />
+              <span className='highlightblue'>dining</span>
+              <Typing.Delay ms={2000} />
+              <Typing.Backspace count={15} />
+              <span className='highlightblue'>golf courses</span>
+              <Typing.Delay ms={2000} />
+              <Typing.Backspace count={15}/>
+            </Typing>
+          );
+              return (
+                  <div>
                 <Nav/>
                 <body>
                     <h1 id='maintitle'>Immerse yourself in <span className='highlightblack'>paradise</span></h1>
@@ -61,24 +70,38 @@ export default class Home  extends Component {
                     </div> 
                     <div id='background1'></div> 
                     <div className='section1'>
-                        <p>
-                    {content()}
-                            Home to clear blue oceans and lush green valleys, West Maui is truly the definition of paradise. Here you’ll find a blend of fascination hawaiian culture, memorable adventures and modern-day conveniences. Experience world-class golf courses, wonderful shopping and unique dining options. For those with a taste for nature and adventure, there are beautiful beaches and breathtaking waterfalls to explore and find. With all there is to do in West Maui, when you come here, you’ll see, hear and feel the spirit of Aloha in every way. From the luxurious Kapalua and Ka’anapali Resorts to historic Lahaina town, paradise and adventure await you at every turn.
-                        </p>
+                        <div className='infobox'>
+                            <img alt=''/>
+                            <h4>150+ Condominiums Underconstruction</h4>
+                            <p></p>
+                            <button className='primarybutton'>Available</button>
+                        </div>
+                        <div className='infobox'>
+                            <img alt=''/>
+                            <h4>20+ Buildings Available</h4>
+                            <p></p>
+                            <button className='primarybutton'>Site Map</button>
+                        </div> 
+                            <div className='infobox'>
+                            <img alt=''/>
+                            <h4>Over 40 Years of Excellence</h4>
+                            <p>Founded by President and CEO, Dale L. Rindlisbacher in 1976. Forty years later, what began as a family business with just two employees, has grown to become a highly reputable property management and development company.</p>
+                            <button className='primarybutton'>Bach Corperation</button>
+                        </div>  
                     </div>
                     <div id='background2'></div>
                     <div className='section2'> 
-                    <div style={{width:'35%'}}>
-                        <h1>Check out <span className='highlightblue'>places</span> near by Wailele Ridge!</h1>
-                        <div className='placesnav'>
-                            <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places === 'all'? 'placesbuttonactive':'placesbutton'}value='all'>All</button>
-                            <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places ===  'beaches'? 'placesbuttonactive' : 'placesbutton'} value='beaches'>Beaches</button>
-                            <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places ===  'parks'? 'placesbuttonactive' : 'placesbutton'} value='parks'>Parks</button>
-                            <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places==='shopping'?'placesbuttonactive':'placesbutton'} value='shopping'>Shopping</button>
-                            <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places==='dining'?'placesbuttonactive':'placesbutton'} value='dining'>Dining</button>
-                            <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places==='golf'?'placesbuttonactive':'placesbutton'} value='golf'>Golf</button>
+                        <div style={{width:'35%'}}>
+                            <h3>Check out {AnimatedTypingComponent()} near Wailele Ridge.</h3>
+                            <div className='placesnav'>
+                                <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places === 'all'? 'primarybuttonactive':'primarybutton'}value='all'>All</button>
+                                <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places ===  'beaches'? 'primarybuttonactive' : 'primarybutton'} value='beaches'>Beaches</button>
+                                <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places ===  'parks'? 'primarybuttonactive' : 'primarybutton'} value='parks'>Parks</button>
+                                <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places==='shopping'?'primarybuttonactive':'primarybutton'} value='shopping'>Shopping</button>
+                                <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places==='dining'?'primarybuttonactive':'primarybutton'} value='dining'>Dining</button>
+                                <button onClick={(e)=>{this.changePlace(e)}} className={this.state.places==='golf'?'primarybuttonactive':'primarybutton'} value='golf'>Golf</button>
+                            </div> 
                         </div> 
-                    </div> 
                             {this.state.places === 'beaches' ?
                                 <img alt='' src={beaches} className='places'/>
                                 :
@@ -107,4 +130,12 @@ export default class Home  extends Component {
         )
     }
 }
+
+
+
+
+
+{/* <p>
+Home to clear blue oceans and lush green valleys, West Maui is truly the definition of paradise. Here you’ll find a blend of fascination hawaiian culture, memorable adventures and modern-day conveniences. Experience world-class golf courses, wonderful shopping and unique dining options. For those with a taste for nature and adventure, there are beautiful beaches and breathtaking waterfalls to explore and find. With all there is to do in West Maui, when you come here, you’ll see, hear and feel the spirit of Aloha in every way. From the luxurious Kapalua and Ka’anapali Resorts to historic Lahaina town, paradise and adventure await you at every turn.
+</p> */}
 
